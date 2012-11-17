4f0fbf7865e8a2425928404a820dbac46f7c7d06
Ariel Mora <morfreek.other@gmail.com>
2012-11-17 04:10:36 -0300

    Avance II

---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+class IndexController extends Zend_Controller_Action{
+    public function init(){
+    	$this->view->active = 1;
+    }
+
+    public function indexAction(){}
+}
+?>
---
+++ application/modules/farmacias/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class Farmacias_IndexController extends Zend_Controller_Action{
+    public function init(){
+    	$this->view->active = 2;
+    }
+    public function indexAction(){}
+}
+?>
